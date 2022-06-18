2b01ead2b99dba60985c240c8e3afe18ab614700
Степан Войтов <razin777@inbox.ru>
2022-06-18 18:40:52 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float ticketPrice = 50000.45F;
-        int milePrice = 20;
-        int milesEarned = (int) ticketPrice / milePrice;
-        System.out.println(milesEarned);
+
+        //Переменные
+        int initialAccount = 100;
+        int depositAmount = 1100;
+        int bonusAmountEach100Rubles = 1;
+        int minDeposit = 1000;
+
+        //Вычисления
+        int bonusRubles;
+        if (depositAmount > minDeposit) {
+            bonusRubles = depositAmount / bonusAmountEach100Rubles / 100;
+        } else {
+            bonusRubles = 0;
+        }
+        int finalAccount = initialAccount + depositAmount + bonusRubles;
+
+        //Вывод результата
+        System.out.println("Вы пополнили счёт на " + depositAmount + " рублей. Вам начислено " + bonusRubles + " бонусных рублей. Теперь на Вашем счету " + finalAccount + " рублей.");
     }
 }
