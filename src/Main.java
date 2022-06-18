b6c865b8d03da358df06f1cef9895b72376961fa
Степан Войтов <razin777@inbox.ru>
2022-06-18 18:24:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 50000.45F;
+        int milePrice = 20;
+        int milesEarned = (int) ticketPrice / milePrice;
+        System.out.println(milesEarned);
+    }
+}
